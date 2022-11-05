9ee206677f49dbe6402d4c7528bab3401c63a38e
maratsadykov <msadykov@i-novus.ru>
2022-11-05 13:02:18 +0300

    initial commit

---
+++ src/test/java/ru/puppetters/sneakersshop/SneakersShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.puppetters.sneakersshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakersShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
